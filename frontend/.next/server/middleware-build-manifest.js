347ced88eb782f4de1c13aa40319152c769dea0b
Toshihiro Nezu <toshihironezu@ToshihironoMacBook-Pro.local>
2025-07-09 11:57:22 +1200

    Configure gitignore and commit changes

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-616e068a201ad621.js","static/chunks/fd9d1056-9f91b5e418130764.js","static/chunks/117-90e22fc57e593a01.js","static/chunks/main-app-553b88b68db1fa74.js"],pages:{"/_app":["static/chunks/webpack-616e068a201ad621.js","static/chunks/framework-6bb7d6253c30bdb1.js","static/chunks/main-3a7d0562c8abf034.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-616e068a201ad621.js","static/chunks/framework-6bb7d6253c30bdb1.js","static/chunks/main-3a7d0562c8abf034.js","static/chunks/pages/_error-7ba65e1336b92748.js"],"/results/[round]":["static/chunks/webpack-616e068a201ad621.js","static/chunks/framework-6bb7d6253c30bdb1.js","static/chunks/main-3a7d0562c8abf034.js","static/chunks/pages/results/[round]-3ea1c5b026b0e57b.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
